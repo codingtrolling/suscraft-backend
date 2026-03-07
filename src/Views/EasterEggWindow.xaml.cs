e5ddf2ebf94f8d45a9428ae4737f85bcd442144f
codingtrolling <rabi65171@gmail.com>
2026-03-07 19:31:20 +0000

    Final: Added Taran Taran Easter Egg and Click Counter

---
+++ src/Views/EasterEggWindow.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+namespace SUSCRAFT.Views {
+    public partial class EasterEggWindow : Window {
+        public EasterEggWindow() { InitializeComponent(); }
+        private void Close_Click(object sender, RoutedEventArgs e) { this.Close(); }
+    }
+}
--- src/Views/MainWindow.xaml.cs
+++ src/Views/MainWindow.xaml.cs
@@ -1,14 +1,28 @@
 using System.Windows;
+using System.Windows.Input;
 using SUSCRAFT.ViewModels;
 
 namespace SUSCRAFT.Views
 {
     public partial class MainWindow : Window
     {
+        private int _clickCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = new MainViewModel();
+        }
+
+        private void Logo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _clickCount++;
+            if (_clickCount >= 10)
+            {
+                _clickCount = 0; // Reset
+                var egg = new EasterEggWindow();
+                egg.ShowDialog();
+            }
         }
     }
 }
